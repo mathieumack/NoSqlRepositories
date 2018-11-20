49eaf3cf229c56a359ea90bed4f2e9c907e178af
MACK Mathieu <mathieu.mack@econocom.com>
2018-11-20 01:44:55 +0100

    Add nuget packaes

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -13,6 +13,9 @@ Set-Location -Path $locationNuspec
 
 $strPath = $location + '\NoSqlRepositories.Core\bin\Release\NoSqlRepositories.Core.dll'
 
+write-host "Update the nuget.exe file" -foreground "DarkGray"
+.\NuGet.exe update -self
+	
 $VersionInfos = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($strPath)
 $ProductVersion = $VersionInfos.ProductVersion
 write-host "Product version : " $ProductVersion -foreground "Green"
@@ -45,6 +48,12 @@ $nuSpecFile =  $locationNuspec + '\NoSqlRepositories.JsonFiles.Net.nuspec'
 Foreach-Object {$_ -replace "{BuildNumberVersion}", "$ProductVersion" } | 
 Set-Content $nuSpecFile
 
+write-host "Update nuspec versions NoSqlRepositories.MongoDb.Net.nuspec"	 -foreground "DarkGray"
+$nuSpecFile =  $locationNuspec + '\NoSqlRepositories.MongoDb.Net.nuspec'
+(Get-Content $nuSpecFile) | 
+Foreach-Object {$_ -replace "{BuildNumberVersion}", "$ProductVersion" } | 
+Set-Content $nuSpecFile
+
 write-host "Generate nuget packages" -foreground "Green"
 
 write-host "Generate nuget package for NoSqlRepositories.Core.nuspec" -foreground "DarkGray"
@@ -55,6 +64,8 @@ write-host "Generate nuget package for NoSqlRepositories.MvvX.JsonFiles.Pcl.nusp
 .\NuGet.exe pack NoSqlRepositories.MvvX.JsonFiles.Pcl.nuspec
 write-host "Generate nuget package for NoSqlRepositories.JsonFiles.Net.nuspec"	 -foreground "DarkGray"
 .\NuGet.exe pack NoSqlRepositories.JsonFiles.Net.nuspec
+write-host "Generate nuget package for NoSqlRepositories.MongoDb.Net.nuspec"	 -foreground "DarkGray"
+.\NuGet.exe pack NoSqlRepositories.MongoDb.Net.nuspec
 
 $apiKey = $env:NuGetApiKey
 	
@@ -71,3 +82,6 @@ write-host NoSqlRepositories.MvvX.JsonFiles.Pcl.$ProductVersion.nupkg -foregroun
 
 write-host NoSqlRepositories.JsonFiles.Net.$ProductVersion.nupkg -foreground "DarkGray"
 .\NuGet push NoSqlRepositories.JsonFiles.Net.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
+
+write-host NoSqlRepositories.MongoDb.Net.$ProductVersion.nupkg -foreground "DarkGray"
+.\NuGet push NoSqlRepositories.MongoDb.Net.$ProductVersion.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
