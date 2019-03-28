1a119bb555506dfc9b94bc10562faf1bbb6db72e
MACK Mathieu <mathieu.mack@econocom.com>
2019-03-28 10:51:40 +0100

    Add unit tests ad .netcore

---
+++ NoSqlRepositories.Tests.Shared/Entities/TestEntity.cs
@@ -0,0 +1,41 @@
+﻿using NoSqlRepositories.Core;
+using System;
+using System.Collections.Generic;
+
+namespace NoSqlRepositories.Tests.Shared.Entities
+{
+    public class TestEntity : IBaseEntity
+    {
+        /// <summary>
+        /// Unique indentifier
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Creation date of the object in the Viewer repository
+        /// </summary>
+        public DateTimeOffset SystemCreationDate { get; set; }
+
+        /// <summary>
+        /// Last update date of the object in the Viewer repository
+        /// </summary>
+        public DateTimeOffset SystemLastUpdateDate { get; set; }
+
+        /// <summary>
+        /// Logical delete status
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        public string Name { get; set; }
+        public DateTimeOffset Birthday { get; set; }
+        public float PoidsFloat { get; set; }
+        public double PoidsDouble { get; set; }
+        public bool IsAMan { get; set; }
+        public int NumberOfChildenInt { get; set; }
+        public long NumberOfChildenLong { get; set; }
+
+        public List<string> Cities { get; set; } = new List<string>();
+
+        public List<TestEntity> Childs { get; set; } = new List<TestEntity>();
+    }
+}
