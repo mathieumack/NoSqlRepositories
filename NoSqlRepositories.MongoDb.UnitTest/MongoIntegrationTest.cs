d1e7543a56313858f4eddc5b1b3e59b245751b8e
MACK Mathieu <mathieu.mack@econocom.com>
2019-05-25 00:17:55 +0200

    Update MongoDb unit tests to use Mongo2Go Update readme.md file in order to add link to Azure Dev ops Pipeline ;)

---
+++ NoSqlRepositories.MongoDb.UnitTest/MongoIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿//using Mongo2Go;
+//using MongoDB.Driver;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace NoSqlRepositories.MongoDb.UnitTest
+//{
+//    public class MongoIntegrationTest
+//    {
+//        internal static MongoDbRunner _runner;
+//        internal static IMongoCollection<TestDocument> _collection;
+
+//        internal static void CreateConnection()
+//        {
+//            _runner = MongoDbRunner.Start();
+
+//        }
+//    }
+//}
